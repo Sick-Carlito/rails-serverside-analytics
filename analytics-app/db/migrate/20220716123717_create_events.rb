66540ddd7396b9f6da52b7a8dc1287d103ed8b37
Sick-Carlito <carlb1319@gmail.com>
2022-07-16 16:05:51 +0300

    Initial commit; created visitor and event models

---
+++ analytics-app/db/migrate/20220716123717_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+
+      t.string :path, null: false
+      t.string :method, null: false
+      t.string :params
+      t.references :visitor, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
